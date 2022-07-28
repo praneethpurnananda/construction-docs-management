85104662ddceea11012ca22e7993fe930e249cfd
Bhaskarpraveen <praveennaidu264@gmail.com>
2022-07-28 19:14:10 +0530

    login changes

--- src/app/backend-api.service.ts
+++ src/app/backend-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient , HttpParams , HttpHeaders} from "@angular/common/http";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CommonApiResponceUiComponent } from './shared-module/common-api-responce-ui/common-api-responce-ui.component';
@@ -9,40 +9,50 @@ import { CommonApiResponceUiComponent } from './shared-module/common-api-responc
 })
 export class BackendApiService {
 
-  constructor(private httpclient:HttpClient,private _snackBar:MatSnackBar) { }
+  URL: string = 'http://localhost:3000/users';
 
-  registerUser(registerform:any){
-    return this.httpclient.post('http://localhost:3000/users/register', registerform,{
-      observe:'body'
+  constructor(private httpclient: HttpClient, private _snackBar: MatSnackBar) { }
+
+  registerUser(registerform: any) {
+    return this.httpclient.post(`${this.URL}/register`, registerform, {
+      observe: 'body'
     })
   }
 
-  loginUser(loginDetails: any){
-    return this.httpclient.post('http://localhost:3000/users/login', loginDetails,{
+  loginUser(loginDetails: any) {
+    return this.httpclient.post(`${this.URL}/login`, loginDetails, {
       observe: 'body',
-      headers: new HttpHeaders().append('x-access-token',  sessionStorage.getItem('token') || '')
+      headers: new HttpHeaders().append('x-access-token', sessionStorage.getItem('token') || '')
     })
   }
 
   //otp
-  Otp(otpform:any){
-    return this.httpclient.post('http://localhost:3000/users/verifyotp', otpform,{
+  Otp(otpform: any) {
+    return this.httpclient.post(`${this.URL}/verifyotp`, otpform, {
       observe: 'body'
     })
   }
-  //internal methods
-  snakBarMethod(message:any,status:boolean){
-    let snackbarClassName = status ? 'success-snackbar' : 'error-snackbar';
-    this._snackBar.openFromComponent(CommonApiResponceUiComponent ,{
-    duration: 5 * 1000,
-          panelClass:['notification-css', snackbarClassName],
-          horizontalPosition:"end",
-          verticalPosition:"top",
-          data:{
-            message: message,
-            status: status
-          }
 
-    } );    
+  //resentOtp
+  resentOtp(userDetails: any) {
+    return this.httpclient.post(`${this.URL}/resendotp`, userDetails, {
+      observe: 'body'
+    })
+  }
+
+  //internal methods
+  snakBarMethod(message: any, status: boolean) {
+    let snackbarClassName = status ? 'success-snackbar' : 'error-snackbar';
+    this._snackBar.openFromComponent(CommonApiResponceUiComponent, {
+      duration: 5 * 1000,
+      panelClass: ['notification-css', snackbarClassName],
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      data: {
+        message: message,
+        status: status
+      }
+
+    });
   }
 }
